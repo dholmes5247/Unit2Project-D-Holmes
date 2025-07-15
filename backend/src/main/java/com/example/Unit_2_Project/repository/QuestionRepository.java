712d13ec91188b57258da3e4fc1f5fd47d057e14
dholmes5247 <dholmes5247@hotmail.com>
2025-07-15 10:01:43 -0500

    still trying to sort out GitHub, QuestionRepository  finished

--- backend/src/main/java/com/example/Unit_2_Project/repository/QuestionRepository.java
+++ backend/src/main/java/com/example/Unit_2_Project/repository/QuestionRepository.java
@@ -1,4 +1,9 @@
 package com.example.Unit_2_Project.repository;
 
-public interface QuestionRepository {
+import com.example.Unit_2_Project.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    // Additional queries added if needed
 }
+
