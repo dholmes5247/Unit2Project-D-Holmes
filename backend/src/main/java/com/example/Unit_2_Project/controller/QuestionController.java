1d380c01bcb5d7611d9d7ba130fb00e5926248ad
dholmes5247 <dholmes5247@hotmail.com>
2025-07-15 10:54:32 -0500

    still trying to sort out GitHub, QuestionController continue

--- backend/src/main/java/com/example/Unit_2_Project/controller/QuestionController.java
+++ backend/src/main/java/com/example/Unit_2_Project/controller/QuestionController.java
@@ -1,4 +1,18 @@
 package com.example.Unit_2_Project.controller;
 
+import java.util.Optional;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import com.example.Unit_2_Project.repository.QuestionRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/questions")
+@CrossOrigin(origins = "*") // Allow frontend access
+
+
 public class QuestionController {
 }
