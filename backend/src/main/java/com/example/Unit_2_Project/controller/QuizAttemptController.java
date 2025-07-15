205f81ec4364c80cab74add78899f6a8c9c9bba5
dholmes5247 <dholmes5247@hotmail.com>
2025-07-15 10:17:57 -0500

    still trying to sort out GitHub, QuizAttemptController Start

--- backend/src/main/java/com/example/Unit_2_Project/controller/QuizAttemptController.java
+++ backend/src/main/java/com/example/Unit_2_Project/controller/QuizAttemptController.java
@@ -1,4 +1,11 @@
 package com.example.Unit_2_Project.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@requestMapping("/api/quiz-attempts")
+
+
 public class QuizAttemptController {
+
 }
