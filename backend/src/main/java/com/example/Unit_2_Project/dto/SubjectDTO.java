16000e7ba520948f0fb297e5549aaeee594133b8
dholmes5247 <dholmes5247@hotmail.com>
2025-07-16 10:24:58 -0500

    starting to add DTOs to fix the error SubjectDTO

--- backend/src/main/java/com/example/Unit_2_Project/dto/SubjectDTO.java
+++ backend/src/main/java/com/example/Unit_2_Project/dto/SubjectDTO.java
@@ -1,4 +1,10 @@
 package com.example.Unit_2_Project.dto;
 
+import lombok.Data;
+
+@Data
 public class SubjectDTO {
+    private String name;
+    private String description;
+    private String imageUrl;
 }
